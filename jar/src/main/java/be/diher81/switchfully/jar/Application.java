6497eb1c799e53b0bcbc22a46a22bf6f84b4a80f
Hermans Dirk (100) <dirk.hermans@cm.be>
2018-03-23 13:10:06 +0100

    poms, springboot. Application runs on server

--- jar/src/main/java/be/diher81/switchfully/jar/Application.java
+++ jar/src/main/java/be/diher81/switchfully/jar/Application.java
@@ -1,4 +1,12 @@
 package be.diher81.switchfully.jar;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "be.diher81.switchfully")
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
